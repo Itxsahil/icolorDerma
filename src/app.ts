48e210277cf1d052ad3ae2d6e6219a961bdd6adc
Sahil Khan <sahilkh9087@gmail.com>
2026-05-12 00:12:03 +0530

    add origin

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'], // your frontend URL
+    origin: ['http://localhost:3000', 'http://localhost:3001', "https://icolorderma-client.vercel.app"], // your frontend URL
     credentials: true, // Allow cookies to be sent
   })
 );
